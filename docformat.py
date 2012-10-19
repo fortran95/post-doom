269a19f795f14955fc1e4a2adec3d35004e136c1
lucifer <fortran95@126.com>
2012-10-20 02:13:38 +0800

    frameworking

---
+++ docformat.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from xipher import *
+
+class EncryptedFile:
+    def __init__(self):
+        pass
+
+    def read(self,src,key_callback,qa_callback):
+        if type(src)==str:
+            # Read Document
+            pass
+
+    def generate(self,key,question_answers,content):
+        
