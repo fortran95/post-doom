dfcfdf2d4a5981f2f93e739716419dec8168751a
lucifer <fortran95@126.com>
2012-10-20 03:21:45 +0800

    add reading, fix a bug that answers may not be associated with questions.

--- docformat.py
+++ docformat.py
@@ -11,8 +11,26 @@ class EncryptedFile:
 
     def read(self,src,key_callback,qa_callback):
         if type(src)==str:
-            # Read Document
-            pass
+            try:
+                constructed = json.loads(src)
+                checkkey = str(constructed['key'])
+                questions = list(constructed['questions'])
+                content = str(constructed['content']).decode('base64')
+            except:
+                return False
+        else:
+            return False
+        key = str(key_callback(u'请输入解密密钥/Enter decrypting key:'))
+        qa = dict(qa_callback(u'请回答这些问题/Answer these questions:',questions))
+        enckey = self._calculate_key(key,qa)
+
+        if Hash('md5',enckey).hexdigest() != checkkey:
+            return False
+
+        try:
+            return x(enckey).decrypt(content)
+        except:
+            return False
 
     def generate(self,key,question_answers,content):
         # The encrypt key is generated as follows:
@@ -25,8 +43,7 @@ class EncryptedFile:
         # with ciphertext, so that it's easy to verify user's input
         # without attempted decrytion.
 
-        answers = question_answers.values()
-        enckey = self._calculate_key(key,answers)
+        enckey = self._calculate_key(key,question_answers)
         timestamp = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
         content = "%s\n\n%s" % (timestamp,content)
         
@@ -34,16 +51,27 @@ class EncryptedFile:
 
         constructed = {
             'key': Hash('md5',enckey).hexdigest(),
-            'q&a': question_answers.keys(),
+            'questions': question_answers.keys(),
             'content': ciphertext.encode('base64').replace('\n',''),
         }
 
         return json.dumps(constructed,indent=4)
 
-    def _calculate_key(self,key,answers):
+    def _calculate_key(self,key,qa):
+        answers = ['%s:%s' % (str(k).strip(),str(qa[k]).strip()) for k in qa.keys()]
         answers.sort()
-        joint_ans = ';'.join([str(each).strip().lower() for each in answers])
+        joint_ans = ';'.join([each.lower() for each in answers])
         return Hash('whirlpool',joint_ans).hmac(key,True) + Hash('sha512',joint_ans).hmac(key,True)
 
 if __name__ == '__main__':
-    print EncryptedFile().generate('This is my very secret key',{'Parent':'1','b':2},'content')
+    docf = EncryptedFile().generate('k',{'Parent':'1','b':'Li '},'content')
+    def askkey(p):
+        return raw_input(p.encode('utf-8'))
+    def askqa(p,q):
+        print p
+        ret = {}
+        for each in q:
+            ret[each] = raw_input(each.encode('utf-8'))
+        return ret
+
+    print EncryptedFile().read(docf,askkey,askqa)
