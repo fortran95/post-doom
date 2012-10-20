fd6f71265131e7a33490ad692707d1546dead62d
lucifer <fortran95@126.com>
2012-10-20 15:47:22 +0800

    Add editor support for Q&As.

--- _add.py
+++ _add.py
@@ -4,23 +4,26 @@ from utils import *
 from gui.editor import editor as e
 
 def handler(gpass,dpath):
-    title, content = '',''
+    title, content, qas = '','',{}
     while True:
-        s = e(title,content,[])
+        s = e(title,content,qas)
         s.showDialog()
         result = s.result
         if result != None:
             # Do a confirm
             title = formatTitle(result['title'])
-            content = result['content']
+            content = str(result['content'])
+            qas = dict(result['qas'])
 
+            clearScreen()
             if (not title) or (not content):
                 print '标题或内容有误'
                 continue
-
-            clearScreen()
             print '即将记录如下信息：'
             print '标题 %s' % title.encode('utf-8')
+            print '问答'
+            for q in qas:
+                print " * [%s] %s" % (q,qas[q])
             print '内容\n%s' % content.encode('utf-8')
             print '--------'
             cmd = raw_input('<Enter>:重新修改 s+<Enter>:保存 其他:取消').strip().lower()
--- gui/editor.py
+++ gui/editor.py
@@ -2,18 +2,25 @@
 from Tkinter import *
 
 class editor:
-    def __init__(self,title='',content='',questions=[]):
+    def __init__(self,title='',content='',questions={}):
         self.title = str(title)
         self.content = str(content)
-        self.questions = str(questions)
+        self.qas = dict(questions)
 
         self._createWidgets()
         self.result = None
 
     def _okcommand(self):
+        qas = {}
+        for qbox,abox in self.qalist:
+            q = qbox.get().strip()
+            a = abox.get().strip()
+            if q and a:
+                qas[q] = a
         self.result = {
             'title': self.titleBox.get().strip(),
             'content': self.contentBox.get(1.0,END).strip(),
+            'qas': qas
         }
         self.root.destroy()
         self.root.quit()
@@ -23,20 +30,42 @@ class editor:
         self.root.title('编辑文件')
         self.root.resizable(0,0)
 
-        self.titleBox = Entry()
+        self.titleBox = Entry(self.root)
         if self.title:
             self.titleBox.insert(END,self.title)
 
-        self.contentBox = Text()
+        self.contentBox = Text(self.root,height=15,width=60)
         if self.content:
             self.contentBox.insert(END,self.content)
+
+        self.qabox = Frame(self.root)
+        self.qabox_p1 = Label(self.qabox,text='问题')
+        self.qabox_p2 = Label(self.qabox,text='答案')
+        self.qabox_p1.grid(row=0,column=0)
+        self.qabox_p2.grid(row=0,column=1)
+        self.qalist = []
+
+        qakeys = self.qas.keys()
+        for i in range(0,4):
+            self.qalist.append((
+                                Entry(self.qabox,width=25),
+                                Entry(self.qabox,width=25)
+                              ))
+            ind = len(self.qalist) - 1
+            self.qalist[ind][0].grid(row=i+1,column=0)
+            self.qalist[ind][1].grid(row=i+1,column=1)
+
+            if i < len(qakeys):
+                self.qalist[ind][0].insert(END,qakeys[i])
+                self.qalist[ind][1].insert(END,self.qas[qakeys[i]])
 
         self.btnOK = Button(text='确认')
         self.btnOK['command'] = self._okcommand
 
         self.titleBox.grid(row=0,column=0,sticky=N+E+W+S)
         self.contentBox.grid(row=1,column=0)
-        self.btnOK.grid(row=2,column=0)
+        self.qabox.grid(row=2,column=0,sticky=N+E+W+S)
+        self.btnOK.grid(row=3,column=0)
 
     def showDialog(self):
         self.root.mainloop()
